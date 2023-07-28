d5266b9d20321ee98c4aa3529fc8f94ce3991a0e
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-07-28 18:53:38 +0100

    inital (wip)

--- main.py
+++ main.py
@@ -2,4 +2,24 @@ from steam import deck_ui
 
 flags = {}
 
+print("starting initial configuration")
+
+game_width = input("Enter Width of game output: ")
+game_height = input("Enter Height of game output: ")
+
+window_width = input("Enter Width of rendered window: ")
+window_height = input("Enter Height of rendered window: ")
+
+upscaling_methods = ()
+
+upscaling = input("Would you like to use an upscaling method?\n1. AMD FidelityFX Super Resolution\n2. NVIDIA Image Scaling\n3. Integer Scaling (Fit)\n4. Stretch Scaling")
+
+flags = {
+  "game_width":game_width,
+  "game_height":game_height,
+  "window_width":window_width,
+  "window_height":window_height,
+  "upscaling":upscaling
+}
+
 deck_ui(flags)
