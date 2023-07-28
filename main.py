b8e6e68adc6c786d1182cfcdf43bbd2492a35556
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-07-28 22:38:59 +0100

    make better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from modules.steam import deck_ui
 
-print("starting initial configuration")
+print("Initializing First-Time Setup")
 
 game_width = input("Enter Width of game output: ")
 game_height = input("Enter Height of game output: ")
@@ -12,32 +12,40 @@ upscaling_methods_list = [
     "AMD FidelityFX Super Resolution",
     "NVIDIA Image Scaling",
     "Integer Scaling",
-    "Stretch Scaling"
+    "Stretch Scaling",
+    "none"
 ]
 
 upscaling_method_number = 0
 upscaling_method_numbers = []
 
 for upscaling_method in upscaling_methods_list:
-    print(upscaling_method)
     upscaling_method_number = upscaling_method_number + 1
     upscaling_method_numbers.append(upscaling_method_number)
+    print(str(upscaling_method_number) + ". " + str(upscaling_method))
 
 upscaling_method = None
 
-while upscaling_method not in upscaling_method_numbers:
+while True:
     upscaling_method = input(f"Enter Upscaling Method (1-{upscaling_method_number}): ")
-    if upscaling_method not in upscaling_method_numbers:
+    upscaling_method_valid = True
+    if not upscaling_method.isnumeric():
         print("Error: Invalid Choice")
-    else:
-        print(f"Upscaling Method: {upscaling_method}")
+        upscaling_method_valid = False
+    if upscaling_method_valid:
+        if int(upscaling_method) not in upscaling_method_numbers:
+            print("Error: Invalid Choice")
+            upscaling_method_valid = False
+        else:
+            print(f"Upscaling Method: {upscaling_methods_list[int(upscaling_method)]}")
+            break
 
 flags = {
     "game_width": game_width,
     "game_height": game_height,
     "window_width": window_width,
     "window_height": window_height,
-    "upscaling": upscaling
+    "upscaling": upscaling_method
 }
 
 deck_ui(flags)
