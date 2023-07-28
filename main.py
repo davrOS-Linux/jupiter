0e2dd07b15b3deede539e3d9a35151ec51f5dd7b
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-07-28 22:12:44 +0100

    Fix PEP-8 violations :skull:

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from modules.steam import deck_ui
 
-flags = {}
-
 print("starting initial configuration")
 
 game_width = input("Enter Width of game output: ")
@@ -11,26 +9,30 @@ window_width = input("Enter Width of rendered window: ")
 window_height = input("Enter Height of rendered window: ")
 
 upscaling_methods_list = [
-  "AMD FidelityFX Super Resolution",
-  "NVIDIA Image Scaling",
-  "Integer Scaling",
-  "Stretch Scaling"
+    "AMD FidelityFX Super Resolution",
+    "NVIDIA Image Scaling",
+    "Integer Scaling",
+    "Stretch Scaling"
 ]
 
-for upscaling_method in upscaling_methods_list:
-  print(upscaling_method)
+upscaling_method_number = 0
 
-upscaling_method = None
+for upscaling_method in upscaling_methods_list:
+    print(upscaling_method)
+    upscaling_method_number = upscaling_method_number + 1
+
 upscaling_method = input("")
 
-upscaling = input("Would you like to use an upscaling method?\n1. AMD FidelityFX Super Resolution\n2. NVIDIA Image Scaling\n3. Integer Scaling (Fit)\n4. Stretch Scaling")
+upscaling = input(
+    "Would you like to use an upscaling method?\n1. AMD FidelityFX Super Resolution\n2. NVIDIA Image Scaling\n3. "
+    "Integer Scaling (Fit)\n4. Stretch Scaling")
 
 flags = {
-  "game_width":game_width,
-  "game_height":game_height,
-  "window_width":window_width,
-  "window_height":window_height,
-  "upscaling":upscaling
+    "game_width": game_width,
+    "game_height": game_height,
+    "window_width": window_width,
+    "window_height": window_height,
+    "upscaling": upscaling
 }
 
 deck_ui(flags)
